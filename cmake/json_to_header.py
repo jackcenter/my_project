832c0ace4e216cedf670aaa4d4e032aeb862f80d
Jack <jack.d.center@gmail.com>
2025-11-23 13:05:09 -0700

    Initial work adding a static config

---
+++ cmake/json_to_header.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import json
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: json_to_header.py input.json output.h")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(input_file, "r") as f:
+    config = json.load(f)
+
+with open(output_file, "w") as f:
+    f.write("#pragma once\n\n")
+    f.write("namespace HardwareConfig {\n")
+    for key, value in config.items():
+        if isinstance(value, str):
+            f.write(f'    constexpr auto {key} = "{value}";\n')
+        else:
+            f.write(f"    constexpr auto {key} = {value};\n")
+    f.write("}\n")
--- lib/mylib/src/hello.cpp
+++ lib/mylib/src/hello.cpp
@@ -1,7 +1,13 @@
 #include "mylib/hello.hpp"
 
+#include <fmt/format.h>
+
+#include <config.h>
+
 namespace mylib {
-    std::string hello() {
-        return "Hello from mylib!";
-    }
+std::string hello() {
+  const double value_1{Config::VALUE_1};
+  return fmt::format(
+      "Hello from mylib! I can read VALUE_1, {}, from the config!", value_1);
 }
+} // namespace mylib
